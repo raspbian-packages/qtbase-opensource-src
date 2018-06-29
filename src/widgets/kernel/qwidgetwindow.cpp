7ae38c42fdac79c039c4b0cc7e719fa167b023d4
Fabian Vogt <fabian@ritter-vogt.de>
2018-06-29 09:15:51 +0200

    Revert "QWidgetWindow: Immediately forward close events to QWindow"
    This reverts commit e0b5ff4ad583befbecbcbe462998e3ed80899531.
    
    References: kde#395988
    References: QTBUG-43344
    
    Gbp-Pq: Name Revert-QWidgetWindow-Immediately-forward-close-event.patch

--- src/widgets/kernel/qwidgetwindow.cpp
+++ src/widgets/kernel/qwidgetwindow.cpp
@@ -237,7 +237,6 @@ bool QWidgetWindow::event(QEvent *event)
     switch (event->type()) {
     case QEvent::Close:
         handleCloseEvent(static_cast<QCloseEvent *>(event));
-        QWindow::event(event);
         return true;
 
     case QEvent::Enter:
