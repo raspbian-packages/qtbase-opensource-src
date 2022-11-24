d8f31b36e89109ab6ca1b3536904ba32ed915f07
Debian Qt/KDE Maintainers <debian-qt-kde@lists.debian.org>
2022-11-24 17:38:09 +0000

    fix recursion crash when calling setStyleSheet with qproperty-styleSheet
    Origin: upstream, https://code.qt.io/cgit/qt/qtbase.git/commit/?id=e9cdcc7cb314586a
    Last-Update: 2021-11-13
    
    When calling setStyleSheet with property qproperty-styleSheet,
    QStyleSheetStyle::polish will call QStyleSheetStyle::setProperties,
    and then QStyleSheetStyle::setProperties goes on to call setProperty.
    Because there is property qproperty-styleSheet, it will update
    stylesheet by calling QStyleSheetStyle::polish.
    This causes the recursive call to crash.
    
    Gbp-Pq: Name fix_recursion_crash.diff

--- src/widgets/styles/qstylesheetstyle.cpp
+++ src/widgets/styles/qstylesheetstyle.cpp
@@ -2649,6 +2649,9 @@ void QStyleSheetStyle::setProperties(QWidget *w)
         default: v = decl.d->values.at(0).variant; break;
         }
 
+        if (propertyL1 == QByteArray("styleSheet") && value == v)
+            continue;
+
         w->setProperty(propertyL1, v);
     }
 }
